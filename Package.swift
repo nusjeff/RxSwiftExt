5752ea88006be8ac84eb4fb0e639da5676f86ae8
nusjeff <jeff@nustechnology.com>
2024-10-25 10:50:36 +0700

    Update config.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "RxSwiftExt", targets: ["RxSwiftExt"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.8.0")),
+        .package(url: "https://github.com/nusjeff/RxSwiftExt.git", .upToNextMajor(from: "5.1.1")),
     ],
     targets: [
         .target(name: "RxSwiftExt", dependencies: ["RxSwift", "RxCocoa"], path: "Source"),
